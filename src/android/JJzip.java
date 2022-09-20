363b942ddddfcbbe7d042426756d3553045bec4a
SmokeNsk <109534426+SmokeNsk@users.noreply.github.com>
2022-09-20 14:57:29 +0700

    Update JJzip.java

--- src/android/JJzip.java
+++ src/android/JJzip.java
@@ -14,49 +14,50 @@ import org.json.JSONObject;
 public class JJzip extends CordovaPlugin {
 
     private enum ACTIONS {
-          zip
-        , unzip
-    };
-    
+        zip, unzip
+    }
+
+    ;
+
     /**
      * Executes the request and returns PluginResult.
      *
-     * @param action            The action to execute.
-     * @param args              JSONArry of arguments for the plugin.
-     * @param callbackContext   The callback id used when calling back into JavaScript.
-     * @return                  True if the action was valid, false if not.
+     * @param action          The action to execute.
+     * @param args            JSONArry of arguments for the plugin.
+     * @param callbackContext The callback id used when calling back into JavaScript.
+     * @return True if the action was valid, false if not.
      */
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
 
         if (action != null) {
             cordova.getThreadPool().execute(() -> {
                 try {
-        boolean result          = true;
-        String actionType       = "";
-        JSONObject validOptions = validOptions(args);
-        
-        if(validOptions==null){
-            this.processResponse(callbackContext, false, "Some parameters were missed - Missed file path -");
-        }
-        
-        switch (ACTIONS.valueOf(action)) {
-            case zip:
-                actionType              = "compress";
-                compressZip makeZip     = new compressZip(validOptions);
-                result                  = makeZip.zip();
-            break;
-            case unzip:
-                actionType              = "decompress";
-                decompressZip unZip     = new decompressZip(validOptions);
-                result                  = unZip.unZip();
-            break;
-            default:
-                JJzip.processResponse(callbackContext, false, "Some parameters were missed - Action not found -");
-                result = false;
-            break;
-        }
+                    boolean result = true;
+                    String actionType = "";
+                    JSONObject validOptions = validOptions(args);
 
-        String msg  = (result)?actionType+" Operation success":actionType+" Operation fail";
+                    if (validOptions == null) {
+                        this.processResponse(callbackContext, false, "Some parameters were missed - Missed file path -");
+                    }
+
+                    switch (ACTIONS.valueOf(action)) {
+                        case zip:
+                            actionType = "compress";
+                            compressZip makeZip = new compressZip(validOptions);
+                            result = makeZip.zip();
+                            break;
+                        case unzip:
+                            actionType = "decompress";
+                            decompressZip unZip = new decompressZip(validOptions);
+                            result = unZip.unZip();
+                            break;
+                        default:
+                            JJzip.processResponse(callbackContext, false, "Some parameters were missed - Action not found -");
+                            result = false;
+                            break;
+                    }
+
+                    String msg = (result) ? actionType + " Operation success" : actionType + " Operation fail";
                     JJzip.processResponse(callbackContext, result, msg);
                     //return result;
                 } catch (Exception ex) {
@@ -73,80 +74,76 @@ public class JJzip extends CordovaPlugin {
     }
 
     /**
-     * 
-     * @param ctx               The plugin CallbackContext
-     * @param success           boolean that define if the JS plugin should fire the success or error function
-     * @param msg               The String msg to by send
+     * @param ctx     The plugin CallbackContext
+     * @param success boolean that define if the JS plugin should fire the success or error function
+     * @param msg     The String msg to by send
      * @throws JSONException
      */
-    private static void processResponse(CallbackContext ctx, boolean success, String msg) throws JSONException{
+    private static void processResponse(CallbackContext ctx, boolean success, String msg) throws JSONException {
         JSONObject response = new JSONObject();
         response.put("success", success);
         response.put("message", msg);
-        if(success){
+        if (success) {
             ctx.success(response);
-        }
-        else{
+        } else {
             ctx.error(response);
         }
     }
-    
+
     /**
-     * 
-     * @param args              The JSONArray sent by the client
-     * @return                  A Valid JSONObject with the need it options to execute or null if the file option path is not set
+     * @param args The JSONArray sent by the client
+     * @return A Valid JSONObject with the need it options to execute or null if the file option path is not set
      * @throws JSONException
      */
-    private JSONObject validOptions(JSONArray args) throws JSONException{
-        
-        JSONObject options  = new JSONObject();
-        String source       = args.optString(0);
-        
-        if(source == null || source.isEmpty()){
+    private JSONObject validOptions(JSONArray args) throws JSONException {
+
+        JSONObject options = new JSONObject();
+        String source = args.optString(0);
+
+        if (source == null || source.isEmpty()) {
             return null;
         }
-        
+
         options.put("source", source);
-        
-        JSONObject extraOptObj  = args.optJSONObject(1);
-        String[] extraOptArr    = new String[]{"target","name"};
-                
+
+        JSONObject extraOptObj = args.optJSONObject(1);
+        String[] extraOptArr = new String[]{"target", "name"};
+
         for (int i = 0; i < extraOptArr.length; i++) {
-            String strOpt   = extraOptObj.optString(extraOptArr[i]);
-            if(strOpt!=null && !strOpt.isEmpty()){
+            String strOpt = extraOptObj.optString(extraOptArr[i]);
+            if (strOpt != null && !strOpt.isEmpty()) {
                 options.put(extraOptArr[i], strOpt);
                 options.put("useExtra", true);
             }
         }
         options = validPaths(options);
-        
+
         return options;
     }
-    
+
     /**
-     * 
      * @param options The passed options merges with the extra params
      * @return A Valid JSONObject with the need it options to execute and the merge paths or null some of the paths are empty
      * @throws JSONException
      */
-    private JSONObject validPaths(JSONObject options) throws JSONException{
-        String sourceEntry  = options.optString("source");
-        String targetPath   = options.optString("target");
-        
-        if(targetPath.isEmpty()){
-            targetPath      = sourceEntry.substring(0, sourceEntry.lastIndexOf("/")+1);
+    private JSONObject validPaths(JSONObject options) throws JSONException {
+        String sourceEntry = options.optString("source");
+        String targetPath = options.optString("target");
+
+        if (targetPath.isEmpty()) {
+            targetPath = sourceEntry.substring(0, sourceEntry.lastIndexOf("/") + 1);
         }
 
-        sourceEntry         = sourceEntry.replace("file://", "");
-        targetPath          = targetPath.replace("file://", "");
-        String sourcePath   = sourceEntry.substring(0, sourceEntry.lastIndexOf("/")+1);
-        String sourceName   = sourceEntry.replace(sourcePath, "");
-        sourceName          = (sourceName.lastIndexOf(".")==-1)?sourceName:sourceName.substring(0, sourceName.lastIndexOf("."));
-        
-        if(sourceEntry.isEmpty() || targetPath.isEmpty() || sourcePath.isEmpty() || sourceName.isEmpty()){
+        sourceEntry = sourceEntry.replace("file://", "");
+        targetPath = targetPath.replace("file://", "");
+        String sourcePath = sourceEntry.substring(0, sourceEntry.lastIndexOf("/") + 1);
+        String sourceName = sourceEntry.replace(sourcePath, "");
+        sourceName = (sourceName.lastIndexOf(".") == -1) ? sourceName : sourceName.substring(0, sourceName.lastIndexOf("."));
+
+        if (sourceEntry.isEmpty() || targetPath.isEmpty() || sourcePath.isEmpty() || sourceName.isEmpty()) {
             return null;
         }
-        
+
         options.put("sourceEntry", sourceEntry);
         options.put("sourcePath", sourcePath);
         options.put("targetPath", targetPath);
